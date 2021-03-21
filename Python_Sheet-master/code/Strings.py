6659bdd955d28b7858fc8b5b1bd60cad691435ea
El-Bando <58035128+El-Bando@users.noreply.github.com>
2021-03-21 17:50:16 +0100

    Init commit

---
+++ Python_Sheet-master/code/Strings.py
@@ -0,0 +1,9 @@
+string = "python"
+string2 = """Lorem ipsum
+dolor sit amet"""
+a = 1234
+for letters in string:  # loops through the string like an array
+    print(letters)
+if "ipsum" in string2:  # search for ipsum in string
+    print("ipsum is in String 2")
+print(2 * "8")  # prints 88
